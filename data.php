09302ad28db17bb426fa9f6fe3e798bc27c1cab7
rahadityaputra <rahadit.a.p123@gmail.com>
2024-03-14 14:53:46 +0700

    menambahkan data password

--- data.php
+++ data.php
@@ -1,35 +1,37 @@
 <?php 
-    $mahasiswa = 
+
+    // data's student
+    $students = 
     [
         [
-            "Nama" => "Rahaditya Abimanyu Putra",
-            "NIM"  => "123230095",
+            "name" => "Rahaditya Abimanyu Putra",
+            "nim"  => "123230095",
             "email" => "rahaditya@gmail.com",
-            "sandi" => "rahaditya123"
+            "password" => "rahaditya123"
         ],
         [
-            "Nama" => "Alfonsus Sitanggang",
-            "NIM"  => "123230100",
+            "name" => "Alfonsus Sitanggang",
+            "nim"  => "123230100",
             "email" => "alfonsus@gmail.com",
-            "sandi" => "rahaditya123"
+            "password" => "rahaditya123"
         ],
         [
-            "Nama" => "Muhammad Abid Dewantoro",
-            "NIM"  => "123230093",
+            "name" => "Muhammad Abid Dewantoro",
+            "nim"  => "123230093",
             "email" => "abid@gmail.com",
-            "sandi" => "rahaditya123"
+            "password" => "rahaditya123"
         ],
         [
-            "Nama" => "Rifki Rahardian",
-            "NIM"  => "123230113",
+            "name" => "Rifki Rahardian",
+            "nim"  => "123230113",
             "email" => "rifki@gmail.com",
-            "sandi" => "rahaditya123"
+            "password" => "rahaditya123"
         ],
         [
-            "Nama" => "Ayodya Enhanayoan",
-            "NIM"  => "123230098",
+            "name" => "Ayodya Enhanayoan",
+            "nim"  => "123230098",
             "email" => "ayodya@gmail.com",
-            "sandi" => "rahaditya123"
+            "password" => "rahaditya123"
         ]
     ]
 
