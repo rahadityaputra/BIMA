7d251bc4cbdbc2cc96afae36d3f36983c6ab2c79
rahadityaputra <rahadit.a.p123@gmail.com>
2024-03-14 14:50:48 +0700

    mengganti file ekstensi html dengan php karena untuk membuat sistem login dengan bantuan bahasa php

---
+++ data.php
@@ -0,0 +1,36 @@
+<?php 
+    $mahasiswa = 
+    [
+        [
+            "Nama" => "Rahaditya Abimanyu Putra",
+            "NIM"  => "123230095",
+            "email" => "rahaditya@gmail.com",
+            "sandi" => "rahaditya123"
+        ],
+        [
+            "Nama" => "Alfonsus Sitanggang",
+            "NIM"  => "123230100",
+            "email" => "alfonsus@gmail.com",
+            "sandi" => "rahaditya123"
+        ],
+        [
+            "Nama" => "Muhammad Abid Dewantoro",
+            "NIM"  => "123230093",
+            "email" => "abid@gmail.com",
+            "sandi" => "rahaditya123"
+        ],
+        [
+            "Nama" => "Rifki Rahardian",
+            "NIM"  => "123230113",
+            "email" => "rifki@gmail.com",
+            "sandi" => "rahaditya123"
+        ],
+        [
+            "Nama" => "Ayodya Enhanayoan",
+            "NIM"  => "123230098",
+            "email" => "ayodya@gmail.com",
+            "sandi" => "rahaditya123"
+        ]
+    ]
+
+?>
--- login.php
+++ login.php
@@ -7,7 +7,7 @@
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
 		<link rel="stylesheet" href="style.css" />
 	</head>
-	<body>
+	<body>				
 		<div class="container-fluid rounded-2 mx-auto mt-5 p-5" style="background-color: white; width: 400px">
 			<div class="row d-flex justify-content-center">
 				<!-- <div class="col-3"></div> -->
