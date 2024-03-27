d0f9a840fcd946274a69b047abc3d6e684d616be
rahadityaputra <rahadit.a.p123@gmail.com>
2024-03-27 16:06:50 +0700

    memperbaiki form sign up

--- signup.php
+++ signup.php
@@ -18,7 +18,7 @@
 			<h3 class="text-center">Sistem Informasi Akademik</h3>
 
 			<!-- form  -->
-			<form action="index.php" method="post">
+			<form action="login.php" method="post">
 				<div class="mb-3">
 					<label for="exampleInputEmail1" class="form-label">Email</label>
 					<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" required/>
@@ -27,12 +27,20 @@
 					<label for="exampleInputPassword1" class="form-label">Password</label>
 					<input type="password" class="form-control bi-eye-slash" id="exampleInputPassword1" name="password" required/>
 				</div>
+				<div class="mb-3">
+					<label for="input-name" class="form-label">Name</label>
+					<input type="text" class="form-control" id="input-name" name="input-name" required/>
+				</div>
+				<div class="mb-3">
+					<label for="input-nim" class="form-label">Nim</label>
+					<input type="text" class="form-control" id="input-nim" name="input-nim" required/>
+				</div>
 				<div class="mb-3 form-check">
 					<input type="checkbox" class="form-check-input" id="exampleCheck1" name="verification"/>
 					<label class="form-check-label" for="exampleCheck1">Saya bukan robot</label>
 				</div>
 				<div class="d-grid gap-2 mb-3">
-					<button class="btn btn-primary" type="submit" name="submit">Log In</button>
+					<button class="btn btn-primary" type="submit" name="submit">Register</button>
 				</div>
 			</form>
 		</div>
